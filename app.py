cc6e6837543b27cfbdad8518be591fde54ef9c14
juar1nascimento <juari.nascimento@serra.es.gov.br>
2026-08-30 12:12:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from streamlit_gsheets import GSheetsConnection
 # CONFIGURAÇÕES E CONSTANTES
 # ==========================================
 ARQUIVO_EXCEL = "Tabela_Patrimonios_UBS_Feu_Rosa.xlsx"
-GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/NOVO_ID_DA_SUA_PLANILHA/edit"
+# Cole aqui a URL exata da sua planilha do Google Sheets
+GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1yzNx6WcCrZbb-KvZCEpFYAuX025p-iy1/edit"
 
 st.set_page_config(
     page_title="Leitor de Código de Barras - UBS Feu Rosa",
