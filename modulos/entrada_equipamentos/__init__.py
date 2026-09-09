c17f1ac718b4a92f104de9445ff434754890f603
juar1nascimento <juari.nascimento@serra.es.gov.br>
2026-09-09 08:13:37 -0300

    Cria módulo independente de Entrada de Equipamentos

---
+++ modulos/entrada_equipamentos/__init__.py
@@ -0,0 +1,3 @@
+from .entrada import atualizar_numero_patrimonio, renderizar_card_entrada, renderizar_sistema_entrada
+
+__all__ = ["atualizar_numero_patrimonio", "renderizar_card_entrada", "renderizar_sistema_entrada"]
