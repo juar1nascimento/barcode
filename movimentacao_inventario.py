694997b9b42eab640cd0aa2ee5560aa54175426c
juar1nascimento <juari.nascimento@serra.es.gov.br>
2026-09-09 12:00:55 -0300

    Limpa import da facade de movimentacao sem perder compatibilidade

--- movimentacao_inventario.py
+++ movimentacao_inventario.py
@@ -1,3 +1,21 @@
 """Compatibilidade: a lógica de movimentação vive em servicos.movimentacao."""
 
-from servicos.movimentacao import *
+from servicos.movimentacao import (
+    COLUNAS_INVENTARIO,
+    _carregar,
+    carregar_dados_excel,
+    salvar_no_excel,
+    registrar_entrada,
+    registrar_saida,
+    excluir_patrimonio_exato,
+)
+
+__all__ = [
+    "COLUNAS_INVENTARIO",
+    "_carregar",
+    "carregar_dados_excel",
+    "salvar_no_excel",
+    "registrar_entrada",
+    "registrar_saida",
+    "excluir_patrimonio_exato",
+]
