2f954336ca310de026199166c8c274e9cd358df7
juar1nascimento <juari.nascimento@serra.es.gov.br>
2026-09-09 08:12:56 -0300

    Cria estrutura modular do Sistema de Inventários

---
+++ modulos/sistema_inventarios/__init__.py
@@ -0,0 +1,23 @@
+from .sistema import (
+    opcoes_tipo_patrimonio,
+    opcoes_setor,
+    validar_tipo_patrimonio,
+    adicionar_e_salvar_sem_sobrescrever,
+    adicionar_e_salvar,
+    processar_imagem,
+    renderizar_card_inventario,
+    renderizar_portal_principal,
+    renderizar_sistema_inventario,
+)
+
+__all__ = [
+    "opcoes_tipo_patrimonio",
+    "opcoes_setor",
+    "validar_tipo_patrimonio",
+    "adicionar_e_salvar_sem_sobrescrever",
+    "adicionar_e_salvar",
+    "processar_imagem",
+    "renderizar_card_inventario",
+    "renderizar_portal_principal",
+    "renderizar_sistema_inventario",
+]
